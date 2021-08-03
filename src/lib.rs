da0670f925e126511447fcd71c5813454529e3d7
Nocoto Day <notcodingtoday@gmail.com>
2021-08-03 23:32:15 +1000

    split out logger library

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,12 @@
 pub extern crate clap;
 pub extern crate log;
 
+#[path = "lib/logger.rs"]
+mod logger;
+
 use clap::{App, AppSettings, Arg, ArgMatches};
-use log::{debug, LevelFilter};
-use log4rs::append::console::{ConsoleAppender, Target};
-use log4rs::append::rolling_file::policy::compound::roll::delete::DeleteRoller;
-use log4rs::append::rolling_file::policy::compound::roll::fixed_window::FixedWindowRoller;
-use log4rs::append::rolling_file::policy::compound::trigger::size::SizeTrigger;
-use log4rs::append::rolling_file::policy::compound::CompoundPolicy;
-use log4rs::append::rolling_file::RollingFileAppender;
-use log4rs::config::{Appender, Config, Root};
-use log4rs::encode::json::JsonEncoder;
-use log4rs::encode::pattern::PatternEncoder;
-use log4rs::filter::threshold::ThresholdFilter;
+use log::debug;
+use log4rs::config::Config;
 
 #[macro_export(app)]
 macro_rules! app {
@@ -117,123 +111,6 @@ pub fn generate_app<'a, 'b>(name: &'b str, description: &'b str, version: &'b st
     .setting(AppSettings::VersionlessSubcommands)
 }
 
-fn str_to_levelfilter(string: &str) -> Result<LevelFilter, String> {
-  match string {
-    "trace" => Ok(LevelFilter::Trace),
-    "debug" => Ok(LevelFilter::Debug),
-    "info" => Ok(LevelFilter::Info),
-    "warn" => Ok(LevelFilter::Warn),
-    "error" => Ok(LevelFilter::Error),
-    _ => Err(format!("Unsupported level filter: {}", string)),
-  }
-}
-
-fn default_logger_config(matches: &ArgMatches) -> (Config, Vec<String>) {
-  let mut config_builder = Config::builder();
-  let mut root_builder = Root::builder();
-  let encoder_string = "{d} {h({l})} [{T}/{t}] {m}{n}";
-  let mut msgs: Vec<String> = Vec::new();
-
-  if !matches.is_present("no-stderr") {
-    let stderr = ConsoleAppender::builder()
-      .target(Target::Stderr)
-      .encoder(Box::new(PatternEncoder::new(encoder_string)))
-      .build();
-    config_builder = config_builder.appender(
-      Appender::builder()
-        .filter(Box::new(ThresholdFilter::new(
-          str_to_levelfilter(matches.value_of("log-level-stderr").unwrap()).unwrap(),
-        )))
-        .build("stderr", Box::new(stderr)),
-    );
-    root_builder = root_builder.appender("stderr");
-  }
-
-  match matches.value_of("log-json") {
-    Some(first_file) => {
-      let pattern = first_file.replace(".0.jsonlog", ".{}.jsonlog");
-      let file_size: u64 = matches
-        .value_of("log-file-size")
-        .unwrap()
-        .parse()
-        .unwrap_or(1000000);
-      let file_count: u32 = matches
-        .value_of("log-json-count")
-        .unwrap()
-        .parse()
-        .unwrap_or(10);
-      let policy = CompoundPolicy::new(
-        Box::new(SizeTrigger::new(file_size)),
-        Box::new(
-          FixedWindowRoller::builder()
-            .build(&pattern, file_count)
-            .unwrap(),
-        ),
-      );
-
-      match RollingFileAppender::builder()
-        .encoder(Box::new(JsonEncoder::new()))
-        .build(&first_file, Box::new(policy))
-      {
-        Ok(file) => {
-          config_builder = config_builder.appender(
-            Appender::builder()
-              .filter(Box::new(ThresholdFilter::new(
-                str_to_levelfilter(matches.value_of("log-level-json").unwrap()).unwrap(),
-              )))
-              .build("json", Box::new(file)),
-          );
-          root_builder = root_builder.appender("json");
-        }
-        Err(e) => msgs.push(format!("Failed to create logger for {}", e)),
-      }
-    }
-    None => msgs.push(String::from("Not logging json file!")),
-  }
-
-  match matches.value_of("log-file") {
-    Some(path) => {
-      let file_size: u64 = matches
-        .value_of("log-file-size")
-        .unwrap()
-        .parse()
-        .unwrap_or(1000000);
-      let policy = CompoundPolicy::new(
-        Box::new(SizeTrigger::new(file_size)),
-        Box::new(DeleteRoller::new()),
-      );
-
-      match RollingFileAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(encoder_string)))
-        .build(path, Box::new(policy))
-      {
-        Ok(file) => {
-          config_builder = config_builder.appender(
-            Appender::builder()
-              .filter(Box::new(ThresholdFilter::new(
-                str_to_levelfilter(matches.value_of("log-level-file").unwrap()).unwrap(),
-              )))
-              .build("file", Box::new(file)),
-          );
-          root_builder = root_builder.appender("file");
-        }
-        Err(e) => msgs.push(format!("Failed to create logger for {}", e)),
-      }
-    }
-
-    None => msgs.push(String::from("Not logging to file!")),
-  }
-
-  (
-    config_builder
-      .build(root_builder.build(
-        str_to_levelfilter(matches.value_of("log-level").unwrap()).unwrap_or(LevelFilter::Debug),
-      ))
-      .unwrap(),
-    msgs,
-  )
-}
-
 pub fn init_logger(matches: &ArgMatches) {
   let mut parse_msg: Vec<String> = Vec::new();
   let mut log4rs_config: Option<Config> = None;
@@ -257,7 +134,7 @@ pub fn init_logger(matches: &ArgMatches) {
   }
 
   if log4rs_config.is_none() {
-    let mut config_and_err = default_logger_config(matches);
+    let mut config_and_err = logger::default_logger_config(matches);
     log4rs_config = Some(config_and_err.0);
     parse_msg.append(&mut config_and_err.1);
   }
---
+++ src/lib/logger.rs
@@ -0,0 +1,129 @@
+use clap::ArgMatches;
+use log::LevelFilter;
+use log4rs::append::console::{ConsoleAppender, Target};
+use log4rs::append::rolling_file::policy::compound::roll::delete::DeleteRoller;
+use log4rs::append::rolling_file::policy::compound::roll::fixed_window::FixedWindowRoller;
+use log4rs::append::rolling_file::policy::compound::trigger::size::SizeTrigger;
+use log4rs::append::rolling_file::policy::compound::CompoundPolicy;
+use log4rs::append::rolling_file::RollingFileAppender;
+use log4rs::config::{Appender, Config, Root};
+use log4rs::encode::json::JsonEncoder;
+use log4rs::encode::pattern::PatternEncoder;
+use log4rs::filter::threshold::ThresholdFilter;
+
+fn str_to_levelfilter(string: &str) -> Result<LevelFilter, String> {
+  match string {
+    "trace" => Ok(LevelFilter::Trace),
+    "debug" => Ok(LevelFilter::Debug),
+    "info" => Ok(LevelFilter::Info),
+    "warn" => Ok(LevelFilter::Warn),
+    "error" => Ok(LevelFilter::Error),
+    _ => Err(format!("Unsupported level filter: {}", string)),
+  }
+}
+
+pub fn default_logger_config(matches: &ArgMatches) -> (Config, Vec<String>) {
+  let mut config_builder = Config::builder();
+  let mut root_builder = Root::builder();
+  let encoder_string = "{d} {h({l})} [{T}/{t}] {m}{n}";
+  let mut msgs: Vec<String> = Vec::new();
+
+  if !matches.is_present("no-stderr") {
+    let stderr = ConsoleAppender::builder()
+      .target(Target::Stderr)
+      .encoder(Box::new(PatternEncoder::new(encoder_string)))
+      .build();
+    config_builder = config_builder.appender(
+      Appender::builder()
+        .filter(Box::new(ThresholdFilter::new(
+          str_to_levelfilter(matches.value_of("log-level-stderr").unwrap()).unwrap(),
+        )))
+        .build("stderr", Box::new(stderr)),
+    );
+    root_builder = root_builder.appender("stderr");
+  }
+
+  match matches.value_of("log-json") {
+    Some(first_file) => {
+      let pattern = first_file.replace(".0.jsonlog", ".{}.jsonlog");
+      let file_size: u64 = matches
+        .value_of("log-file-size")
+        .unwrap()
+        .parse()
+        .unwrap_or(1000000);
+      let file_count: u32 = matches
+        .value_of("log-json-count")
+        .unwrap()
+        .parse()
+        .unwrap_or(10);
+      let policy = CompoundPolicy::new(
+        Box::new(SizeTrigger::new(file_size)),
+        Box::new(
+          FixedWindowRoller::builder()
+            .build(&pattern, file_count)
+            .unwrap(),
+        ),
+      );
+
+      match RollingFileAppender::builder()
+        .encoder(Box::new(JsonEncoder::new()))
+        .build(&first_file, Box::new(policy))
+      {
+        Ok(file) => {
+          config_builder = config_builder.appender(
+            Appender::builder()
+              .filter(Box::new(ThresholdFilter::new(
+                str_to_levelfilter(matches.value_of("log-level-json").unwrap()).unwrap(),
+              )))
+              .build("json", Box::new(file)),
+          );
+          root_builder = root_builder.appender("json");
+        }
+        Err(e) => msgs.push(format!("Failed to create logger for {}", e)),
+      }
+    }
+    None => msgs.push(String::from("Not logging json file!")),
+  }
+
+  match matches.value_of("log-file") {
+    Some(path) => {
+      let file_size: u64 = matches
+        .value_of("log-file-size")
+        .unwrap()
+        .parse()
+        .unwrap_or(1000000);
+      let policy = CompoundPolicy::new(
+        Box::new(SizeTrigger::new(file_size)),
+        Box::new(DeleteRoller::new()),
+      );
+
+      match RollingFileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(encoder_string)))
+        .build(path, Box::new(policy))
+      {
+        Ok(file) => {
+          config_builder = config_builder.appender(
+            Appender::builder()
+              .filter(Box::new(ThresholdFilter::new(
+                str_to_levelfilter(matches.value_of("log-level-file").unwrap()).unwrap(),
+              )))
+              .build("file", Box::new(file)),
+          );
+          root_builder = root_builder.appender("file");
+        }
+        Err(e) => msgs.push(format!("Failed to create logger for {}", e)),
+      }
+    }
+
+    None => msgs.push(String::from("Not logging to file!")),
+  }
+
+  (
+    config_builder
+      .build(root_builder.build(
+        str_to_levelfilter(matches.value_of("log-level").unwrap()).unwrap_or(LevelFilter::Debug),
+      ))
+      .unwrap(),
+    msgs,
+  )
+}
