1ddedd1dc535202284979f8dbe510c82b228c3bd
Nocoto Day <notcodingtoday@gmail.com>
2022-06-11 21:03:17 +0900

    fix test

--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -3,13 +3,12 @@ extern crate eggricesoy;
 
 #[test]
 fn create_app() {
-  let app = eggricesoy::app();
+  let app = eggricesoy::app!();
   assert_eq!(app.get_name(), "eggricesoy");
 }
 
 #[test]
 fn test_app() {
-  let app = eggricesoy::app();
-  let matches = app.get_matches_from(["eggricesoy", "--log4rs-config", "test.yaml"]);
-  eggricesoy::init_logger(&matches);
+  let app = eggricesoy::app!();
+  let matches = eggricesoy::init_app(app).matches;
 }
